f3fefda1462138077541c2438b857e4fab41c64c
Dylan Gore <hello@dylangore.ie>
2017-04-18 13:30:28 +0100

    Fixed naming error

--- inc/dashboard.php
+++ inc/dashboard.php
@@ -3,7 +3,7 @@
     //TODO add options to ask which modules should be removed
 
     // Admin Bar Button
-    function dylangore_support_button($wp_admin_bar){
+    function materialize_fc_support_button($wp_admin_bar){
         $args = array(
         'id' => 'dylangore-support-button',
         'title' => 'Theme Support',
@@ -15,7 +15,7 @@
     $wp_admin_bar->add_node($args);
     }
 
-    add_action('admin_bar_menu', 'dylangore_support_button', 10000);
+    add_action('admin_bar_menu', 'materialize_fc_support_button', 10000);
 	//Remove Admin Bar Links
 	function remove_admin_bar_links() {
 		global $wp_admin_bar;
@@ -29,26 +29,26 @@
 	add_action( 'wp_before_admin_bar_render', 'remove_admin_bar_links' );
 
     // Add Dashboard Widgets
-    function dylangore_dashboard_widgets() {
+    function materialize_fc_dashboard_widgets() {
         global $wp_meta_boxes;
 
-        wp_add_dashboard_widget('dylangore_theme_info_widget', 'Theme Information', 'dylangore_dashboard_theme_info');
-		wp_add_dashboard_widget('dylangore_latest_news_widget', 'Latest News', 'dylangore_dashboard_latest_news');
+        wp_add_dashboard_widget('materialize_fc_theme_info_widget', 'Theme Information', 'materialize_fc_dashboard_theme_info');
+		wp_add_dashboard_widget('materialize_fc_latest_news_widget', 'Latest News', 'materialize_fc_dashboard_latest_news');
     }
 
-	function dylangore_dashboard_theme_info() {
+	function materialize_fc_dashboard_theme_info() {
         $theme_data = wp_get_theme();
         echo '<p>You are running the <b>' . $theme_data->get( 'Name' ) . '</b> Theme' .  ' version <b>' . $theme_data->get( 'Version' ) . '</b></p>';
     }
 
-    function dylangore_dashboard_latest_news() {
+    function materialize_fc_dashboard_latest_news() {
         $theme_data = get_theme_data( get_theme_root() . '/' . 'crazycarts-child' . '/style.css' );
 		$news_text = file_get_contents('https://dylangore.space/news.txt');
         echo($news_text);
     }
 
     // Remove Default Dashboard Widgets
-    function dylangore_dashboard_remove_defaults() {
+    function materialize_fc_dashboard_remove_defaults() {
         remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
         remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
         remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
@@ -59,8 +59,8 @@
         remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
         remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');//since 3.8
     }
-        add_action( 'admin_init', 'dylangore_dashboard_remove_defaults' );
-        add_action('wp_dashboard_setup', 'dylangore_dashboard_widgets');
+        add_action( 'admin_init', 'materialize_fc_dashboard_remove_defaults' );
+        add_action('wp_dashboard_setup', 'materialize_fc_dashboard_widgets');
 
 
 ?>
