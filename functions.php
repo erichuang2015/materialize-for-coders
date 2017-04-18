f6345564332a996dff09c2d00b66ad203287ecb8
Dylan Gore <hello@dylangore.ie>
2017-04-18 13:32:40 +0100

    Switched to minified versions of Materialize

--- functions.php
+++ functions.php
@@ -6,12 +6,12 @@
     //Define styles and scripts
     function materializefc_styles_scripts() {
         wp_enqueue_style( 'material-icons', '//fonts.googleapis.com/icon?family=Material+Icons');
-        wp_enqueue_style( 'materialize', get_stylesheet_directory_uri() . '/css/materialize.css');
+        wp_enqueue_style( 'materialize', get_stylesheet_directory_uri() . '/css/materialize.min.css');
         wp_enqueue_style( 'site', get_stylesheet_directory_uri() . '/css/site.css');
         wp_enqueue_style( 'animate', get_stylesheet_directory_uri() . '/css/animate.min.css');
 
         wp_enqueue_script( 'jquery'); //This is already included in Wordpress Core, no need for link
-        wp_enqueue_script( 'materialize', get_template_directory_uri() . '/js/materialize.js', $deps = array(), $ver = false, $in_footer = true);
+        wp_enqueue_script( 'materialize', get_template_directory_uri() . '/js/materialize.min.js', $deps = array(), $ver = false, $in_footer = true);
         wp_enqueue_script( 'site', get_template_directory_uri() . '/js/site.js', $deps = array(), $ver = false, $in_footer = true);
     }
 
